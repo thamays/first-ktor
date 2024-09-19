b8fa5544432cc5393feab7d71a17712e7c4dad14
Thamays <gthamays@gmail.com>
2024-09-18 23:38:19 -0400

    Register error handlers

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-status-pages")
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/main/kotlin/com/thamays/plugins/Routing.kt
+++ src/main/kotlin/com/thamays/plugins/Routing.kt
@@ -3,15 +3,25 @@ package com.thamays.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
     routing {
         staticResources("/content", "mycontent")
 
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
         }
 
         get("/test1") {
